fed13e5ee1bc5722d69b6866144c05c2814a8ddc
Irene-24 <okaforirenen@gmail.com>
2018-11-24 21:35:47 +0100

    more edits to verify functions

--- scripts/create.js
+++ scripts/create.js
@@ -1,9 +1,8 @@
 const fields ={};
-const inputslabels = document.querySelectorAll("label");
+const form = document.querySelector("form");
+const inputslabels = form.querySelectorAll("label");
 console.log(`i have ${inputslabels.length} labels`);
-
 inputslabels.forEach((el)=>fields[el.attributes.for.value] = [false,0]);
-
 
 
 const panels = document.querySelectorAll("form > div");
@@ -22,6 +21,57 @@ console.log(`I have ${panels.length} panels`)
 
 buttons[0].addEventListener("click",prev,false);
 buttons[1].addEventListener("click", next, false);
+
+function VerifyAll() 
+{
+
+    let invalidFields = [];
+    let arr = [];
+    let start = 0;
+    let length = panels[0].querySelectorAll("label").length;
+    if (panel == 0)
+    {
+        arr = panels[0].querySelectorAll("label") ;        
+       
+    }
+    else if (panel == 1) 
+    {
+        arr = panels[1].querySelectorAll("label");
+        start = length;
+        length = length + arr.length ;
+      
+    }
+    else if(panel == 2) 
+    {
+        arr = panels[2].querySelectorAll("label");
+        start = panels[1].querySelectorAll("label").length;
+        length = length + arr.length;
+           
+    }
+
+    for (let index = start; index < length; index++) 
+    {
+        if (fields[arr[index].attributes.name.value][0] == false) 
+        {
+            invalidFields.push[fields[arr[index].attributes.name.value]];
+        }
+
+    }
+
+    return invalidFields.length;
+
+}
+
+function Verify(event) 
+{
+    //get nodename
+    //see testing.html for how to add evtlistener in practice-4.html
+
+    
+    
+}
+
+
 
 
 function next() 
@@ -83,8 +133,4 @@ function ShowPanel()
     
 }
 
-function Validate()  // will push all to person
-{
-
-}
 
